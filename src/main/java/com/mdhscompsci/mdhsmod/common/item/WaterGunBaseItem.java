2fcd9f0bec21418db19d6cc3f8833d7f79c38dcd
User-12691296 <94016358+User-12691296@users.noreply.github.com>
2022-02-10 17:53:41 -0500

    Fixed some setup things

--- src/main/java/com/mdhscompsci/mdhsmod/common/item/WaterGunBaseItem.java
+++ src/main/java/com/mdhscompsci/mdhsmod/common/item/WaterGunBaseItem.java
@@ -3,10 +3,13 @@ package com.mdhscompsci.mdhsmod.common.item;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 
@@ -14,29 +17,35 @@ public class WaterGunBaseItem extends Item {
     private float damage;
     private byte tier;
 
-    public WaterGunBaseItem (float damage, byte tier) {
-        super(new Item.Properties());
+    public WaterGunBaseItem (float damage, int tier) {
+        super(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT).rarity((tier<=3)?  Rarity.COMMON : Rarity.UNCOMMON));
         this.damage = damage;
-        this.tier = tier;
+        this.tier = (byte) tier;
     }
 
     @Override
     public InteractionResult interactLivingEntity (ItemStack pStack, Player pPlayer, LivingEntity pInteractionTarget, InteractionHand pUsedHand) {
-        return InteractionResult.PASS;
+        try {
+            pInteractionTarget.hurt(DamageSource.indirectMobAttack(pPlayer, pPlayer), this.damage);
+        } catch (Exception e) {
+            return InteractionResult.FAIL;
+        }
+
+        showWaterLaser(pPlayer, pInteractionTarget.getEyePosition(), (byte) (3^this.tier));
+        return InteractionResult.SUCCESS;
     }
 
-    protected void showWaterLaser (Player player, LivingEntity target, byte laser_density) {
+    protected void showWaterLaser (Player player, Vec3 target, byte laser_density) {
         //Start and end pos of the water laser beam
         Vec3 laser_start_pos = player.getEyePosition().subtract(0.0, 0.4, 0.0);
-        Vec3 laser_end_pos = target.getEyePosition();
 
         //Length of the laser
-        double laser_length = laser_start_pos.distanceTo(laser_end_pos);
+        double laser_length = laser_start_pos.distanceTo(target);
 
         //Distance between each water particle in the beam
-        Vec3 laser_particle_increment_distance = (laser_end_pos.subtract(laser_start_pos))
+        Vec3 laser_particle_increment_distance = (target.subtract(laser_start_pos))
                 .normalize()
-                .multiply(1/laser_density, 1/laser_density, 1/laser_density);
+                .multiply(1.0/laser_density, 1.0/laser_density, 1.0/laser_density);
 
         //lc is a simple position storage, a particle is always displayed at it, and it is incremented by the increment each tick
         Vec3 lc = laser_start_pos;
--- src/main/java/com/mdhscompsci/mdhsmod/core/init/ItemInit.java
+++ src/main/java/com/mdhscompsci/mdhsmod/core/init/ItemInit.java
@@ -1,12 +1,18 @@
 package com.mdhscompsci.mdhsmod.core.init;
 
 import com.mdhscompsci.mdhsmod.MdhsMod;
+import com.mdhscompsci.mdhsmod.common.item.WaterGunBaseItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
     private ItemInit() {}
 
     public static final DeferredRegister<Item> ITEMS= DeferredRegister.create(ForgeRegistries.ITEMS, MdhsMod.MOD_ID);
+
+
+    public static final RegistryObject<Item> NETHERITE_WATER_GUN = ITEMS.register("netherite_water_gun", () ->
+            new WaterGunBaseItem(8.0f, 4));
 }
