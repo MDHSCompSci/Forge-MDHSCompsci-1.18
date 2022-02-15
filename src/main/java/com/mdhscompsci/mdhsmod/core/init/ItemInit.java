6df43353b63df3f1376f20558350ba74d912dbc1
User-12691296 <94016358+User-12691296@users.noreply.github.com>
2022-02-15 15:53:34 -0500

    Fixed some setup things

--- src/main/java/com/mdhscompsci/mdhsmod/core/init/ItemInit.java
+++ src/main/java/com/mdhscompsci/mdhsmod/core/init/ItemInit.java
@@ -13,6 +13,17 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS= DeferredRegister.create(ForgeRegistries.ITEMS, MdhsMod.MOD_ID);
 
 
+    //Adding water gun items
+    public static final RegistryObject<Item> WOODEN_WATER_GUN = ITEMS.register("wooden_water_gun", () ->
+            new WaterGunBaseItem(0.0f, 1, 4.0));
+    public static final RegistryObject<Item> STONE_WATER_GUN = ITEMS.register("stone_water_gun", () ->
+            new WaterGunBaseItem(2.0f, 1, 6.0));
+    public static final RegistryObject<Item> IRON_WATER_GUN = ITEMS.register("iron_water_gun", () ->
+            new WaterGunBaseItem(3.0f, 2, 8.0));
+    public static final RegistryObject<Item> GOLD_WATER_GUN = ITEMS.register("gold_water_gun", () ->
+            new WaterGunBaseItem(3.0f, 2, 27.0));
+    public static final RegistryObject<Item> DIAMOND_WATER_GUN = ITEMS.register("diamond_water_gun", () ->
+            new WaterGunBaseItem(5.0f, 3, 12.0));
     public static final RegistryObject<Item> NETHERITE_WATER_GUN = ITEMS.register("netherite_water_gun", () ->
-            new WaterGunBaseItem(8.0f, 4));
+            new WaterGunBaseItem(8.0f, 4, 20.0));
 }
